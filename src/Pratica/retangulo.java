65998dc54124107f3fe5894a4ee658087cea6f29
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-29 12:17:46 -0300

    Leitura da altura com validacao

--- src/Pratica/retangulo.java
+++ src/Pratica/retangulo.java
@@ -20,6 +20,15 @@ public class retangulo {
 			largura = sc.nextDouble();
 		}
 
+		System.out.print("Digite a altura do retangulo: ");
+		double altura = sc.nextDouble();
+
+		while (altura <= 0.0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			altura = sc.nextDouble();
+
+		}
+
 		sc.close();
 
 	}
